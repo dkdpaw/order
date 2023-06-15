bc8b80310ba76167f48f5b9cdc6b48dae8ee15e7
dkdpaw <135975609+dkdpaw@users.noreply.github.com>
2023-06-15 22:00:26 +0900

    commit

---
+++ frontend/src/components/UpdateStockCommand.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            UpdateStock
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="updateStock"
+            >
+                UpdateStock
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'UpdateStockCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            updateStock() {
+                this.$emit('updateStock', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
---
+++ order/src/main/java/order/domain/Order.java
@@ -0,0 +1,51 @@
+package order.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import order.OrderApplication;
+import order.domain.OrderCanceled;
+import order.domain.OrderPlaced;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long orderId;
+
+    private String productId;
+
+    private String customerId;
+
+    private Integer qty;
+
+    private String productName;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderCanceled orderCanceled = new OrderCanceled(this);
+        orderCanceled.publishAfterCommit();
+        // Get request from Order
+        //order.external.Order order =
+        //    Application.applicationContext.getBean(order.external.OrderService.class)
+        //    .getOrder(/** mapping value needed */);
+
+    }
+
+    @PrePersist
+    public void onPrePersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
