1aeb750c520b2bb32842dba770ccb41077a38454
dkdpaw <135975609+dkdpaw@users.noreply.github.com>
2023-06-15 21:49:24 +0900

    commit

--- order/src/main/java/order/external/OrderService.java
+++ order/src/main/java/order/external/OrderService.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(
-    name = "order",
-    url = "${api.url.order}",
-    fallback = OrderServiceImpl.class
-)
+@FeignClient(name = "order", url = "${api.url.order}")
 public interface OrderService {
     @RequestMapping(method = RequestMethod.POST, path = "/orders")
     public void placeOrder(@RequestBody Order order);
